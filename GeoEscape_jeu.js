5207cb3701812ee8dc632f7c034664aab93287fa
LilianWecker <117586303+LilianWecker@users.noreply.github.com>
2022-11-09 18:14:23 +0100

    Add files via upload

---
+++ GeoEscape_jeu.js
@@ -0,0 +1,23 @@
+
+
+let paris = [48.85, 2.35];
+let map = L.map('map').setView(paris, 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let mark = new L.LayerGroup();
+map.addLayer(mark);
+
+let marker = L.marker([48.85, 2.35]);
+
+map.on('zoom', function(){
+    var zoom = map.getZoom();
+    if (zoom > 14){
+        marker.addTo(mark);
+    } else{
+        mark.clearLayers();
+    }
+})
+
