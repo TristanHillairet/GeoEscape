047f8dbf79e66a3b2b43c881c7e5c5498bf4a4e8
LilianWecker <lwecker1@gmail.com>
2022-11-19 19:39:07 +0100

    MAJ 19 inutile

--- GeoEscape_jeu.js
+++ GeoEscape_jeu.js
@@ -3,6 +3,15 @@
 let amsterdam = [52.36674, 4.92621];
 let map = L.map('map').setView(amsterdam, 13);
 
+// var mapOptions = {
+//   center : [52.36674, 4.92621];
+//   zoom: 7;
+// }
+// var map = new L.map('map', mapOptions); //Creation d'un objet map
+
+// var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+// map.addLayer(layer); // ajout du layer à la map
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
@@ -22,43 +31,48 @@ fetch('objets.php', {
   let lon = r[0]['lon'];
   let zoom_min = r[0]['zoom_min'];
   let id = r[0]['id_objet'];
-  map.on('zoom', afficher_marker(lat,lon,zoom_min,id));
+  map.on('zoom', function(){
+    afficher_marker(lat,lon,zoom_min,id);
+  });
 })
 
-let layer = new L.LayerGroup();
-map.addLayer(layer);
+var layerGroup = new L.layerGroup();
+layerGroup.addTo(map);
 
 function afficher_marker(lat,lon,zoom_min,id){
-  let mark = L.marker([lat,lon]);
-  let layerGroup = L.layerGroup([mark]);
-  layerGroup.addTo(map);
+  var mark = new L.Marker([lat,lon], {icon: icon_statut});
+  // let layerGroup = L.layerGroup([mark]);
+  // layerGroup.addTo(map);
   var zoom = map.getZoom();
+  console.log(zoom);
   if (zoom > zoom_min){
-    mark.addTo(layer);
-  } else{
-    layerGroup.removeLayer(layer);
+    layerGroup.addLayer(mark);
+  } else {
+    console.log(lat)
+    layerGroup.removeLayer(mark);
   }
 }
 
 
 
-// var icon_statut = L.icon({
-//     iconUrl: 'image_icon/statue-of-liberty.png',
-//     // iconShadow:
-//     iconSize:     [56, 56], // size of the icon
-//     // shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [25, 25], // point of the icon which will correspond to marker's location
-//     // shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
-// })
+ var icon_statut = L.icon({
+    iconUrl: 'image_icon/statue-of-liberty.png',
+    // iconShadow:
+    iconSize:     [56, 56], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [25, 25], // point of the icon which will correspond to marker's location
+    // shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
+})
 
 // let marker_ams = L.marker([52.36674, 4.92621], {icon: icon_statut})
 // map.on('zoom', function(){
 //     var zoom = map.getZoom();
+//     console.log(zoom);
 //     if (zoom > 14){
-//         marker_ams.addTo(mark);
+//         marker_ams.addTo(layer);
 //     } else{
-//         mark.clearLayers();
+//         layer.clearLayers();
 //     }
 // })
 
