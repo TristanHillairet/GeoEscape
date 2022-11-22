7b35b69a5fd3fb7dc618db4bb55dc55ca83ab07e
Tristan Hillairet <hillairettristan@gmail.com>
2022-11-22 16:58:32 +0100

    MAJ PENDANT LE JAVA

--- GeoEscape_jeu.js
+++ GeoEscape_jeu.js
@@ -1,41 +1,21 @@
+//                                      GEOESCAPE_JEU.JS
+// L.WECKER et T.HILLAIRET
+// 11/2022
+// Définition et utilité du fichier
 
 
-let amsterdam = [52.36674, 4.92621];
-let map = L.map('map').setView(amsterdam, 13);
+/*AFFICHAGE DE LA CARTE*/
 
-// var mapOptions = {
-//   center : [52.36674, 4.92621];
-//   zoom: 7;
-// }
-// var map = new L.map('map', mapOptions); //Creation d'un objet map
-
-// var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-// map.addLayer(layer); // ajout du layer à la map
+let paris = [48.857527, 2.351466];/*Point de départ*/
+let map = L.map('map').setView(paris, 13);/*Initialisation de la carte au point de départ*/
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map);/*Chargement du fond openstreetmap*/
 
-
-var layerGroup_10 = new L.layerGroup();
-layerGroup_10.addTo(map);
-
-var layerGroup_14 = new L.layerGroup();
-layerGroup_14.addTo(map);
-
-var layerGroup_16 = new L.layerGroup();
-layerGroup_16.addTo(map);
+/*RECHERCHE DANS LA BDD*/
 
 var data = 'id='+1;
-
-//on va remplir des listes de latitudes, longitudes, ... des objets avec leur id pour pouvoir les traiter par la suite 
-
-let lat = [];
-let lon = [];
-let zoom_min = [];
-
-// on va chercher les infos des objets de la base de donnée avec le fetch
-
 fetch('objets.php', {
   method: 'post',
   body: data,
@@ -46,59 +26,100 @@ fetch('objets.php', {
 .then(r => r.json())
 .then(r => {
   for(let i=0;i<12;i++){
-    let id = r[i]['id_objet'];
-    lat.push([r[i]['lat'],id]);
-    lon.push([r[i]['lon'],id]);
-    zoom_m=r[i]['zoom_min'];
-    console.log(zoom_m);
-    //let id_icone = r[i]['id_icone'];
-    let debut = r[i]['debut'];
+    var id = r[i]['id_objet'];/*Récupère l'id de l'objet*/
+    var lat = r[i]['lat'];/*Récupère la latitde de l'objet*/
+    var lon = r[i]['lon']/*Récupère la longitude de l'objet*/
+    var zoom_m = r[i]['zoom_min'];/*Récupère le niveau de zoom à partir duquel l'objet s'affiche*/
+    var takable_status = r[i]['takable_status'];/*Récupère l'information si l'objet est récupérable*/
+    var id_icone = r[i]['id_icone'];/*Récupère l'id de l'icone de l'objet*/
+    var popup = r[i]['popup'];/*Récupère le Popup de l'objet*/
+    /*POTENTIELLEMENT REFAIRE UN FETCH POUR ALLER CHERCHER L'ICONE*/
+    var open_status = r[i]['open_status'];/*Récupère l'information si l'objet est affichable ou non*/
 
-    if (debut == 1){                      // on va creer les marqueurs dans des layergroupe en fonction de leur zoom_min 
-      if (zoom_min == 10){
-        creer_marker_10(lat,lon);
-      } else if (zoom_min == 14){
-        creer_marker_14(lat,lon);
-      } else if (zoom_min == 16){
-        creer_marker_16(lat,lon);
+    if (open_status == true){
+      if (zoom_m == 10){
+        createMarker10(lat,lon,icone,popup,takable_status);/*cf création markers*/
+      } else if (zoom_m == 14){
+        createMarker14(lat,lon,icone,popup,takable_status);/*cf création markers*/
+      } else if (zoom_m == 16){
+        createMarker16(lat,lon,icone,popup,takable_status);/*cf création markers*/
       }
     }
     map.on('zoom', function(){
-      console.log(map.getZoom());
-      console.log(layerGroup_10.getLayers())
-      afficher_marker(mark,zoom_min);
+      printMarker(marker,zoom_m);
     });
   }
 })
 
-console.log(lat);
+/*CREATION DES MARKERS DE LEAFLET*/
 
-function creer_marker_10(lat,lon){
-  var mark = new L.Marker([lat,lon], {icon: icon_statut});
-  layerGroup_10.addLayer(mark);
+var layergroup_10 = new L.layerGroup();/*Création du Layer au niveau de zoom minimum 10*/
+var layergroup_14 = new L.layerGroup();/*Création du Layer au niveau de zoom minimum 14*/
+var layergroup_16 = new L.layerGroup();/*Création du Layer au niveau de zoom minimum 16*/
+
+//Création d'un marker de niveau de zoom minimum 10
+function createMarker10(lat,lon,icone,popup,takable_status){
+  var marker = new L.Marker([lat,lon], {
+              icon: icon_statut,/*Transfert de l'icone de l'objet au marker*/
+              isTakable: takable_status,/*Transfert du status de prenabilité au marker*/
+              opacity: 0.0/*Par défaut le marker n'est pas visible*/
+             });
+  marker.bindPopup(popup);
+  layergroup_10.addLayer(marker);/*Ajout du marker au layer10*/
 }
 
-function creer_marker_14(lat,lon){
-  var mark = new L.Marker([lat,lon], {icon: icon_statut});
-  layerGroup_14.addLayer(mark);
+//Création d'un marker de niveau de zoom minimum 14
+function createMarker14(lat,lon,icone,popup,takable_status){
+  var marker = new L.Marker([lat,lon], {
+              icon: icone,/*Transfert de l'icone de l'objet au marker*/
+              isTakable: takable_status,/*Transfert du status de prenabilité au marker*/
+              opacity: 0.0/*Par défaut le marker n'est pas visible*/
+            });
+  marker.bindPopup(popup);
+  layergroup_14.addLayer(marker);/*Ajout du marker au layer14*/
 }
 
-function creer_marker_16(lat,lon){
-  var mark = new L.Marker([lat,lon], {icon: icon_statut});
-  layerGroup_16.addLayer(mark);
+//Création d'un marker de niveau de zoom minimum 16
+function createMarker16(lat,lon,icone,popup,takable_status){
+  var marker = new L.Marker([lat,lon], {
+              icon: icone,/*Transfert de l'icone de l'objet au marker*/
+              isTakable: takable_status,/*Transfert du status de prenabilité au marker*/
+              opacity: 0.0/*Par défaut le marker n'est pas visible*/
+            });
+  marker.bindPopup(popup);
+  layergroup_16.addLayer(marker);/*Ajout du marker au layer16*/
 }
 
-
-function afficher_marker(mark,zoom_min){
-  var zoom = map.getZoom();
-  if (zoom > zoom_min){
-    mark.setOpacity(1);
-  } else {
-    mark.setOpacity(0);
+//Affichage des markers de niveau de zoom correspondant
+function printMarker(marker,zoom){
+  var zmap = map.getZoom();/*Récupère le zoom actuel de la carte*/
+  if (zmap > zoom){
+    marker.setOpacity(1.0);/*Si le zoom de la map est supérieur au zoom minimum de l'objet il s'affiche*/
   }
 }
 
+//Evenement quand la souris passe dessus un marqueur
+marker.on('mouseover',function(e){
+  marker.openPopup();/*Ouverture du Popup du marker*/
+});
 
+//Evenement de clic sur un marqueur
+marker.on('click',function(e){
+  if (marker.isTakable == true){
+      /*RECUPERATION DE L'OBJET DANS L'INVENTAIRE*/
+  }
+  else {
+      alert("Objet non récupérable");
+  }
+});
+
+/*UTILISATIOIN D'UN OBJET DE L'INVENTAIRE*/
+
+/*DEBLOQUER UN OBJET*/
+
+/*CHRONOMETRE*/
+
+/*
 
 var icon_statut = L.icon({
   iconUrl: 'image_icon/statue-of-liberty.png',
@@ -109,43 +130,6 @@ var icon_statut = L.icon({
   // shadowAnchor: [4, 62],  // the same for the shadow
   popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
 });
-// let marker_ams = L.marker([52.36674, 4.92621], {icon: icon_statut})
-// map.on('zoom', function(){
-//     var zoom = map.getZoom();
-//     console.log(zoom);
-//     if (zoom > 14){
-//         marker_ams.addTo(layer);
-//     } else{
-//         layer.clearLayers();
-//     }
-// })
 
 
-
-// coord moulin d'amsterdam : 52.36674, 4.92621
-
-
-//                                  INVENTAIRE
-
-//Récupération d'un objet dans l'inventaire 
-
-var circle = L.circle([0, 0]);
-var click_lat = 0;
-var click_lon = 0;
-function onMapClick(e) {
-    //On get les coordonnées du click sur la carte
-    click_lat = e.latlng.lat;
-    click_lon = e.latlng.lng;
-    circle = L.circle([click_lat, click_lon], {radius: 10});
-    console.log('lat = '+click_lat+' lon = '+click_lon);
-}
-map.on('click',onMapClick);
-
-//  1.2- on vérifie que le click n'est pas à plus de 10 m d'un objet récupérable
-
-//  1.3- Si le click est à moins de 10m d'un objet on créer un élément dans la div inventaire pour y placer l'objet
-
-//2-Utilisation d'un objet dans l'inventaire
-// 2.1-On drag & drop l'objet sur la carte
-// 2.2-Si l'objet nécéssitant l'utilisation est affiché on enlève l'objet de l'inventaire
-// 2.3-On dévérouille l'objet débloquable
+*/
