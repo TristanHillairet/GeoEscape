92a35986c7f94b532bd7f800164887cae36a909c
LilianWecker <lwecker1@gmail.com>
2022-12-03 15:57:13 +0100

    MAJ fin du jeu

--- objets.php
+++ objets.php
@@ -4,35 +4,6 @@
 
 include("connect.php");
 
-// if (isset($_POST['data'])){
-//   /* RECUPERATION DES OBJETS */ 
-
-//   $query = "SELECT * FROM objet AS o JOIN icone AS i ON o.id_icone=i.id_icone";
-
-//   //$object_query = "SELECT * FROM objet WHERE id_objet=$id";
-
-//   //$icone_query = "SELECT * FROM icone WHERE id_icone=(SELECT id_icone FROM objet WHERE id_objet=$id)";
-
-//   $objet = mysqli_query($link, $query );
-//   $objets = [];
-
-//   $icone = mysqli_query($link, $icone_query);
-//   $icones = [];
-
-
-//   /* TRANSFROMATION EN JSON */
-
-//   if(isset($objet) && isset($icone)){
-//     while($rows1 = mysqli_fetch_assoc($objet)){
-//       $objets[]=$rows1;
-//     }
-//     echo json_encode($objets,JSON_NUMERIC_CHECK);
-    
-//   }elseif(!isset($objet) || !isset($icone)){
-//     echo "La requete n'est pas bonne";
-//   }  
-// }
-
 if (isset($_POST['id_code_debloque'])){
   /* Recuperation infos objets bloqués */
 
